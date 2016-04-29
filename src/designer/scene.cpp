4692977b37713bf071837d4e63c8cd83053f28e4
clemens <clemens.degasper@gmail.com>
2016-04-29 14:29:20 +0200

    added LJ simulation finished method which adds all generated particles to the grid and clear LJ simulation

--- src/designer/scene.cpp
+++ src/designer/scene.cpp
@@ -2,3 +2,13 @@
 
 Scene::Scene() {
 }
+
+void Scene::LJSimulationFinished()
+{
+    // remove particles from current simulation and adds
+    // them scene grid
+    for(int i = 0; i< this->Sim->x.size();i++){
+        this->addParticleToNonGrid(point{this->Sim->x[i],this->Sim->y[i]});
+    }
+    this->Sim->clear();
+}
